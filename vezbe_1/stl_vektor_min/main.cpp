02366c039bb21dfe4c8efd1db058812eee139221
Nikwiza <nikola.kerleta1@gmail.com>
2022-05-08 01:24:26 +0200

    Doing the first exercise folder...

--- vezbe_1/stl_vektor_min/main.cpp
+++ vezbe_1/stl_vektor_min/main.cpp
@@ -9,10 +9,31 @@ Podrazumeva se: a.size()==b.size()
 */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+typedef vector<int>::const_iterator IT;
+
+vector<int> min(const vector<int> &a, const vector<int> &b){
+    IT ita;
+    IT itb;
+    vector<int> resenje;
+
+    for(ita = a.begin(), itb = b.begin(); ita!=a.end(); ita++, itb++){
+        resenje.push_back(min(*ita, *itb));
+    }
+    return resenje;
+}
 
 int main()
 {
+    vector<int> a = {3, 4, 2, 15, 66};
+    vector<int> b = {1, 5, 777, 5, 662};
+    vector<int> c = min(a,b);
+    IT it;
+    for(it = c.begin(); it!=c.end(); it++){
+        cout<<*it<<" ";
+    }
     return 0;
 }

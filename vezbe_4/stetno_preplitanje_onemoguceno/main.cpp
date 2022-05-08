60890b1cbbd54e1e04e833cda6afad07475bd819
Nikwiza <nikola.kerleta1@gmail.com>
2022-05-09 00:12:21 +0200

    Doing the file vezbe_4

--- vezbe_4/stetno_preplitanje_onemoguceno/main.cpp
+++ vezbe_4/stetno_preplitanje_onemoguceno/main.cpp
@@ -1,7 +1,34 @@
 /*
-Napraviti konkurentni program koji u funkciji visina pita korisnika koliko je visok. Nakon toga korisnik unosi svoju visinu. Na kraju u funkciji se ispisuje uneta visina.
+Napraviti konkurentni program koji u funkciji visina pita korisnika koliko je visok.
+ Nakon toga korisnik unosi svoju visinu. Na kraju u funkciji se ispisuje uneta visina.
 
 Sprečiti štetno preplitanje na terminalu korišćenjem klase mutex.
 
 Kreirati 2 niti od date funkcije. Ispratiti ispis.
 */
+#include<stdio.h>
+#include<iostream>
+#include<mutex>
+#include <thread>
+
+using namespace std;
+mutex m;
+
+void visina(){
+    float visina;
+    unique_lock<mutex> l(m);
+    cout<<"Koja vam je visina? :";
+    cin>>visina;
+    cout<<endl<<"Vasa visina je: "<<visina<<endl;
+}
+
+int main(){
+    thread t(visina);
+    thread t1(visina);
+    t.join();
+    t1.join();
+    return 0;
+
+}
+
+
